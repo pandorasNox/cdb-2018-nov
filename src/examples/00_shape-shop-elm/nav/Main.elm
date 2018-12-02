8b95bc6bed6974aa50d697f1cbfe81b1de6b50c0
pandorasNox <itspandorasnox@gmail.com>
2018-12-02 23:37:33 +0100

    add basic nav example

--- src/examples/00_shape-shop-elm/nav/Main.elm
+++ src/examples/00_shape-shop-elm/nav/Main.elm
@@ -1,7 +1,8 @@
 
 import Browser
-import Browser.Navigation
-import Html exposing (Html, button, div, text, ul, li)
+import Browser.Navigation as Nav
+import Html exposing (Html, button, div, text, ul, li, a, b)
+import Html.Attributes exposing (href)
 import Html.Events exposing (onClick)
 import Json.Decode exposing (Decoder, map2, field, int, string)
 import Http
@@ -17,36 +18,32 @@ main =
     , update = update
     , subscriptions = subscriptions
     , view = view
-    , onUrlChange = urlChanges
-    , onUrlRequest = linkClicked
+    , onUrlChange = UrlChanged
+    , onUrlRequest = LinkClicked
   }
 
 
--- init : #Browser.Navigation.Key# -> #Url.Url# -> flags -> ( model, Cmd msg )
---     , #onUrlChange# : Url.Url -> msg
---     , #onUrlRequest# : Browser.UrlRequest -> msg
+-- urlChanges : Url.Url -> Msg
+-- urlChanges _ =
+--   NoOp
 
-urlChanges : Url.Url -> Msg
-urlChanges _ =
-  NoOp
-
-linkClicked : Browser.UrlRequest -> Msg
-linkClicked _ =
-  NoOp
+-- linkClicked : Browser.UrlRequest -> Msg
+-- linkClicked _ =
+--   NoOp
 
 
 -- MODEL
 
 
 type alias Model = {
-    nav_key: Browser.Navigation.Key,
+    nav_key: Nav.Key,
     nav_url: Url.Url,
     counter : Int
     , products : List Product
   }
 
 
-init : Flags -> Url.Url -> Browser.Navigation.Key -> (Model, Cmd Msg)
+init : Flags -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
 init _ key url =
   (Model url key 0 [], getProducts)
 
@@ -58,6 +55,8 @@ type Msg = Increment
   | Decrement
   | Reset
   | ProductsResult (Result Http.Error (List Product))
+  | LinkClicked Browser.UrlRequest
+  | UrlChanged Url.Url
   | NoOp
 
 
@@ -79,6 +78,17 @@ update msg model =
     ProductsResult (Err err) ->
       (model, Cmd.none)
 
+    LinkClicked urlRequest ->
+      case urlRequest of
+        Browser.Internal url ->
+          ( model, Nav.pushUrl model.nav_key (Url.toString url) )
+
+        Browser.External href ->
+          ( model, Nav.load href )
+
+    UrlChanged url ->
+      ( { model | nav_url = url }, Cmd.none)
+
     NoOp ->
       (model, Cmd.none)
 
@@ -97,25 +107,34 @@ subscriptions model =
 view : Model -> Browser.Document Msg
 view model =
   let
-    body =
+    productsSection =
       if length(model.products) == 0 then
-        [
-          div []
-            [ button [ onClick Decrement ] [ text "-" ]
-            , div [] [ text (String.fromInt model.counter) ]
-            , button [ onClick Increment ] [ text "+" ]
-            , button [ onClick Reset ] [ text "reset" ]
-            , div [] [ text "couldn't load products" ]
-            ]
-        ]
+        div [] [ text "couldn't load products" ]
       else
+        div [] [(productsView model)]
+
+    linkArea =
+      div []
+      [text "The current URL is: "
+      , b [] [ text (Url.toString model.nav_url) ]
+      , ul []
+          [ viewLink "/home"
+          , viewLink "/profile"
+          , viewLink "/reviews/the-century-of-the-self"
+          , viewLink "/reviews/public-opinion"
+          , viewLink "/reviews/shah-of-shahs"
+          ]
+      ]
+
+    body =
         [
           div []
             [ button [ onClick Decrement ] [ text "-" ]
             , div [] [ text (String.fromInt model.counter) ]
             , button [ onClick Increment ] [ text "+" ]
             , button [ onClick Reset ] [ text "reset" ]
-            , div [] [(productsView model)]
+            , productsSection
+            , linkArea
             ]
         ]
   in
@@ -123,6 +142,9 @@ view model =
   , body = body
   }
 
+viewLink : String -> Html msg
+viewLink path =
+  li [] [ a [ href path ] [ text path ] ]
 
 productsView : Model -> Html Msg
 productsView model =
